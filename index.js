b61ff91706ff53b8ac7ad8412dc2604e2fbd20b4
Victor Emanuel <mrgoodye@gmail.com>
2022-05-01 12:37:19 -0300

    dynamic port

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const http = require('http')
 const fs = require('fs')
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html' })
   fs.createReadStream('./index.html').pipe(res)
 })
 
-server.listen(3000)
+server.listen(PORT)
 console.log("eu subi aqui mano");
